766d8b4650d0da8d0d321d7eb211f9388a54f454
bachlx01 <73922823+bachlx01@users.noreply.github.com>
2020-11-23 16:17:09 +0700

    add checkWin

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-let b = document.getElementById("carogame");
-let player = 1;
+// This is program make game caro 10 x 10. This is develope from excercise Tic tac toe in BP, unit Array 2.
 
+// DECLARE THE MAIN VARIABLES.
+let b = document.getElementById("carogame");
+let player = 1;  // this variable change player after each of turn.
 let board = new Array(10);
-let data = "";
+let data = ""; // this variable save string HTML to innerHTML at finally.
 for (i = 0; i < 10; i++) {
     board[i] = new Array('', '', '', '', '', '', '', '', '', '');
 }
-data += "<table>";
+
+//Make the table game from array 'board'
+data += '<table>';
 for (let i = 0; i < board.length; i++) {
     data += "<tr>";
     for (let j = 0; j < board[i].length; j++) {
@@ -17,6 +21,7 @@ for (let i = 0; i < board.length; i++) {
 data += '</table>';
 b.innerHTML = data;
 
+// Function get position of cell and change the value to O or X.
 function changeValue(element) {
     if (player == 1) {
         let positionX = element.getAttribute('id').slice(0, element.getAttribute('id').indexOf('-'));
@@ -37,7 +42,9 @@ function changeValue(element) {
         } else {
             alert('Vị trí này đã được đánh dấu rồi. Nhập lại vị trí khác!');
         }
-        player = 2;
+        player = 2; // After this function excutive, change the current player.
+
+        checkWin();
     } else {
         let positionX = element.getAttribute('id').slice(0, element.getAttribute('id').indexOf('-'));
         let positionY = element.getAttribute('id').slice(element.getAttribute('id').indexOf('-') + 1);
@@ -58,5 +65,21 @@ function changeValue(element) {
             alert('Vị trí này đã được đánh dấu rồi. Nhập lại vị trí khác!');
         }
         player = 1;
+        checkWin();
+    }
+}
+
+function checkWin() {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++){
+            if (board[i][j] != '' && board[i][j] == board[i][j+1] && board[i][j]  == board[i][j+2] )
+                alert('You Win!');
+        }
+    }
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++){
+            if (board[i][j] != '' && board[i][j] == board[i+1][j+1] && board[i][j]  == board[i+2][j+2] )
+                alert('You Win!');
+        }
     }
 }
